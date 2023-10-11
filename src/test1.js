95d4afe70d621f0ee9207f048bbe2953be1a0b6c
zelkovaria <hyein396@gmail.com>
2023-10-11 15:51:34 +0900

    sum 함수 추가

---
+++ src/test1.js
@@ -0,0 +1,18 @@
+//페이지 연달아 있는지 확인 -> return -1
+//시작 면이나 마지막 면이 나오도록 -> [0,1], [401, ?] 안됨
+//배열 이름은 pobi, crong 길이는 2
+
+//페이지 자릿수 더하기와 곱하기 기능
+
+function problem1(pobi, crong) {
+  const sum = (num) => {
+    const stringNum = num.toString();
+
+    let result = 0;
+    for (let i = 0; i < stringNum.length; i++) {
+      result += parseInt(stringNum[i]);
+    }
+    return result;
+  };
+  console.log(sum(125));
+}
