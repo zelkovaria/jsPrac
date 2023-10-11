079a75e812fe8dbb57548b079be126fdb72c8bba
zelkovaria <hyein396@gmail.com>
2023-10-11 17:03:47 +0900

    sum 함수 변경

--- src/test1.js
+++ src/test1.js
@@ -4,22 +4,24 @@
 
 //페이지 자릿수 더하기와 곱하기 기능
 
-function problem1(pobi, crong) {
-  const sum = (num) => {
-    const stringNum = num.toString();
-    let result = 0;
-    for (let i = 0; i < stringNum.length; i++) {
-      result += parseInt(stringNum[i]);
-    }
-    return result;
-  };
+// function problem1(pobi, crong) {
+const sum = (num) => {
+  //   const stringNum = num.toString();
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    const digit = parseInt(num[i]);
+    result += digit;
+  }
+  return result;
+};
 
-  const multiply = (num) => {
-    let result = 1;
-    for (let i = 0; i < num.length; i++) {
-      const digit = parseInt(num[i]);
-      result *= digit;
-    }
-    return result;
-  };
-}
+const multiply = (num) => {
+  let result = 1;
+  for (let i = 0; i < num.length; i++) {
+    const digit = parseInt(num[i]);
+    result *= digit;
+  }
+  return result;
+};
+console.log(sum("125"));
+// }
