4e6e0c26abd5d0dec43754e629451f24da910961
zelkovaria <hyein396@gmail.com>
2023-10-14 22:55:21 +0900

    baseball 폴더 구조 및 md 작성

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // const compareScore = require("./page-cal/calPage");
-const delAlpha = require("./cryptogram-del/delCryptogram");
 
 // const pobi = [97, 98];
 // const crong = [197, 198];
@@ -10,6 +9,10 @@ const delAlpha = require("./cryptogram-del/delCryptogram");
 
 /* #################### */
 
+const delAlpha = require("./cryptogram-del/delCryptogram");
+
 const cryptogram = "zyelleyz";
 const result2 = delAlpha.delAlpha(cryptogram);
 console.log(result2.join(""));
+
+/* #################### */
